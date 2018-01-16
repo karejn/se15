a0f2d0ceb3286f544b1edc346a0c86a1c5e53429
karejn <ek.zhuravleva@gmail.com>
2018-01-16 14:38:10 +0100

    Add constructors for all classes. Make Person class abstract. Modify the method getJobBeschreibung() and override it in child classes. Remove not necessary BERUF enum.

---
+++ src/main/java/at/aau/se15/bsp2/Tester.java
@@ -0,0 +1,23 @@
+package at.aau.se15.bsp2;
+
+public class Tester extends Person {
+
+    public Tester(String vorname, String nachname, Integer alter, String bevorzugtesTestFramework) {
+        super(vorname, nachname, alter);
+        this.bevorzugtesTestFramework = bevorzugtesTestFramework;
+    }
+
+    private String bevorzugtesTestFramework;
+
+    public String getBevorzugtesTestFramework() {
+        return bevorzugtesTestFramework;
+    }
+
+    public void setBevorzugtesTestFramework(String bevorzugtesTestFramework) {
+        this.bevorzugtesTestFramework = bevorzugtesTestFramework;
+    }
+
+    public String getJobBeschreibung() {
+        return this.getClass().getSimpleName();
+    }
+}
