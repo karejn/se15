1db9e47a0fccf616466929d713d8e101ab11bd78
karejn <ek.zhuravleva@gmail.com>
2018-01-16 14:23:03 +0100

    Split Person up to child classes: Entwickler, Architekt and Tester. Change String feld to enum TAETIGKEITSFELD and move to Architekt class

--- src/main/java/at/aau/se15/bsp2/Person.java
+++ src/main/java/at/aau/se15/bsp2/Person.java
@@ -17,26 +17,6 @@ public class Person {
         this.beruf = beruf;
     }
 
-    public void setBevorzugteProgrammierSprache(String bevorzugteProgrammierSprache) {
-        this.bevorzugteProgrammierSprache = bevorzugteProgrammierSprache;
-    }
-
-    public void setIde(IDE ide) {
-        this.ide = ide;
-    }
-
-    public void setDatenbanken(boolean datenbanken) {
-        this.datenbanken = datenbanken;
-    }
-
-    public void setFeld(String feld) {
-        this.feld = feld;
-    }
-
-    public void setBevorzugtesTestFramework(String bevorzugtesTestFramework) {
-        this.bevorzugtesTestFramework = bevorzugtesTestFramework;
-    }
-
     public String getVorname() {
 
         return vorname;
@@ -54,35 +34,12 @@ public class Person {
         return beruf;
     }
 
-    public String getBevorzugteProgrammierSprache() {
-        return bevorzugteProgrammierSprache;
-    }
-
-    public IDE getIde() {
-        return ide;
-    }
-
-    public boolean isDatenbanken() {
-        return datenbanken;
-    }
-
-    public String getFeld() {
-        return feld;
-    }
-
-    public String getBevorzugtesTestFramework() {
-        return bevorzugtesTestFramework;
-    }
 
     private String vorname;
     private String nachname;
     private Integer alter;
     private BERUF beruf;
-    private String bevorzugteProgrammierSprache;
-    private IDE ide;
-    private boolean datenbanken;
-    private String feld;
-    private String bevorzugtesTestFramework;
+
 
     public enum BERUF {
         ENTWICKLER,
@@ -90,18 +47,17 @@ public class Person {
         TESTER
     }
 
-    public enum IDE {
-        ECLIPSE,
-        INTELLIJ,
-        NANO
-    }
 
     public String getJobBeschreibung() {
-        switch(beruf) {
-            case ENTWICKLER: return "Entwickelt Code.";
-            case ARCHITEKT: return "Designed die Architektur des Projekts.";
-            case TESTER: return "Testet Code.";
-            default: return "";
+        switch (beruf) {
+            case ENTWICKLER:
+                return "Entwickelt Code.";
+            case ARCHITEKT:
+                return "Designed die Architektur des Projekts.";
+            case TESTER:
+                return "Testet Code.";
+            default:
+                return "";
         }
     }
 }
